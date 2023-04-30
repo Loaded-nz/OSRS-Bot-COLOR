06c642c91dece2a0fa23dd9861621a812c563161
Loaded-nz <130443238+Loaded-nz@users.noreply.github.com>
2023-05-01 02:13:15 +1200

    init

--- src/model/osrs/__init__.py
+++ src/model/osrs/__init__.py
@@ -5,3 +5,4 @@ from .WillowsDad.WDDegrimer import OSRSWDDegrimer
 from .WillowsDad.WDFishing import OSRSWDFishing
 from .WillowsDad.WillowsDad_bot import WillowsDadBot
 from .WillowsDad.WDMining import OSRSWDMining
+from .molchbot import ZarosMolchBot
