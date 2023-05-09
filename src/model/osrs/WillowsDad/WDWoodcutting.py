f62758d456a310b1b2930fd3b82abe62f471cb35
Loaded-nz <130443238+Loaded-nz@users.noreply.github.com>
2023-05-09 14:52:49 +1200

    Try

--- src/model/osrs/WillowsDad/WDWoodcutting.py
+++ src/model/osrs/WillowsDad/WDWoodcutting.py
@@ -95,6 +95,7 @@ class OSRSWDWoodcutting(WillowsDadBot):
                 # check if inventory is full
                 if self.api_m.get_is_inv_full():
                     self.bank_or_drop(deposit_slots)
+                    
 
                 # Check if idle
                 if self.api_m.get_is_player_idle():
@@ -264,7 +265,8 @@ class OSRSWDWoodcutting(WillowsDadBot):
             self.open_bank()
             time.sleep(self.random_sleep_length())
             self.check_deposit_all()
-            self.deposit_items(deposit_slots, self.deposit_ids)
+            deposit_all_img = self.WILLOWSDAD_IMAGES.joinpath("bank_all.png")
+            self.deposit_items(deposit_all_img, deposit_slots, self.deposit_ids)
             time.sleep(self.random_sleep_length())
             self.close_bank()
         else:
