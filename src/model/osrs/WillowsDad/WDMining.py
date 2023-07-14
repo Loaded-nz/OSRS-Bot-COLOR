f53f0d61455917f6e66e2982c39b0e4b9489f9a0
Loaded-nz <130443238+Loaded-nz@users.noreply.github.com>
2023-07-15 02:21:42 +1200

    Sand

--- src/model/osrs/WillowsDad/WDMining.py
+++ src/model/osrs/WillowsDad/WDMining.py
@@ -292,10 +292,10 @@ class OSRSWDMining(WillowsDadBot):
             time.sleep(self.random_sleep_length()/2)
             self.close_bank()
         else:
-            if Grinder := self.get_nearest_tag(clr.PINK):
+            if Grinder := self.get_nearest_tag(clr.YELLOW):
                 self.mouse.move_to(Grinder.random_point())
                 while not self.mouse.click(check_red_click=True):
-                    if Grinder := self.get_nearest_tag(clr.PINK):
+                    if Grinder := self.get_nearest_tag(clr.YELLOW):
                         self.mouse.move_to(Grinder.random_point())
                     time.sleep(self.random_sleep_length()/2)
 
