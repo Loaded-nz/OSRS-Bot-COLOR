eef41e5e05c00fda7b3fcb898cb9928943fa6f1c
Loaded-nz <130443238+Loaded-nz@users.noreply.github.com>
2023-07-23 02:18:07 +1200

    fix

--- src/model/osrs/WillowsDad/WDMining.py
+++ src/model/osrs/WillowsDad/WDMining.py
@@ -258,7 +258,6 @@ class OSRSWDMining(WillowsDadBot):
                     if Mining_spot := self.get_nearest_tag(clr.PINK):
                         self.mouse.move_to(Mining_spot.random_point())
                 self.api_m.wait_til_gained_xp("Mining", 5)
-                self.sleep(0.1, 0.3)
 
             else:
                 if int(time.time() - self.idle_time) > 10:
