4efcdac09ddfefc76f916b18ea38655e1e2ec3ec
Loaded-nz <130443238+Loaded-nz@users.noreply.github.com>
2023-07-26 10:40:43 +1200

    Karam

--- src/model/osrs/WillowsDad/WDKarams.py
+++ src/model/osrs/WillowsDad/WDKarams.py
@@ -13,7 +13,7 @@ from utilities.geometry import RuneLiteObject
 
 class OSRSKarams(WillowsDadBot):
     def __init__(self):
-        bot_title = "WillowsDad Fishing"
+        bot_title = "Karams"
         description = """Fishes at supported locations."""
         super().__init__(bot_title=bot_title, description=description)
         # Set option variables below (initial value is only used during UI-less testing)
